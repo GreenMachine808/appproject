94752abf26cb1b259c49cfe7d2b679d30feeadc3
GreenMachine808 <mherrera23@punahou.edu>
2016-06-30 13:48:12 -1000

    Continued on the game.
    Continued on the game and made another cube.

--- js/index.js
+++ js/index.js
@@ -1,11 +1,26 @@
 var forms = [
-	"<img src=../assets/rock.jpg>",
-	"<img src=../assets/paper.jpg>",
-	"<img src=../assets/scissors.jpg>"
+	"<img src=assets/rock.jpg>",
+	"<img src=assets/paper.jpg>",
+	"<img src=assets/download.jpeg>"
 ]
 
 
-var show = function() {
-	getElementById("player1").inner.HTML=forms[count];
-	document.getElementById('player1').src = "assets";
+// var show = function() {
+// 	getElementById("player1").inner.HTML= forms[count];
+// 	document.getElementById('player1').src = "assets";
+// }
+
+function leftclick() {
+	var player1choice = Math.floor(Math.random()* forms.length);
+	document.getElementById('player1').innerHTML = forms[player1choice];
+
+
+
+}
+function rightclick() {
+	var player2choice = Math.floor(Math.random()* forms.length);
+	document.getElementById('player2').innerHTML = forms[player2choice];
+
+
+
 }
